fa386fec60a13d876a6b8bfdc09b0da9e8e089ee
Deng Jian <118713625+deng0004@users.noreply.github.com>
2023-02-01 09:50:21 +0100

    Reference from youtube
    youtube url: https://www.youtube.com/watch?v=bBQnWcbxDo8

---
+++ real-time-chat-room/src/main/java/com/example/real/time/chat/room/RealTimeChatRoomApplication.java
@@ -0,0 +1,13 @@
+package com.example.real.time.chat.room;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeChatRoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealTimeChatRoomApplication.class, args);
+	}
+
+}
---
+++ real-time-chat-room/src/test/java/com/example/real/time/chat/room/RealTimeChatRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.real.time.chat.room;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeChatRoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
